66fa854c579df891d2a08df56c6e91c17e6faf37
dinaAlsaid <dinaAlsaid@github.com>
2020-11-01 22:30:05 +0200

    caps logs

--- driver.js
+++ driver.js
@@ -7,11 +7,11 @@ events.on('pickup',(payload)=>pickupHandler(payload));
 function pickupHandler(payload){
   setTimeout(()=>{
     console.log(`DRIVER: picked up ${payload.orderId}`);
-    events.emit('in-transit',{payload})
+    events.emit('in-transit',payload)
   },1000);
 
   setTimeout(()=>{
     console.log('delivered');
-    events.emit('delivered',{payload});
-  },3000)
+    events.emit('delivered',payload);
+  },3000);
 }
--- vendor.js
+++ vendor.js
@@ -2,17 +2,18 @@
 
 const events = require('./events.js');
 const faker = require('faker');
+require('dotenv').config();
 
 const storeName = process.env.STORE;
-//storeName, orderId, customerName, address
+
 let payload = {};
+events.on('delivered',()=>{console.log('thank you');})
 
 setInterval(() => {
   payload.storeName=storeName;
   payload.orderId=faker.random.uuid();
   payload.customerName=faker.name.findName();
   payload.address=faker.address.city();
-  events.emit('pickup',{payload});
+  events.emit('pickup',payload);
 }, 5000);
 
-events.on('delivered',()=>{console.log('thank you');})
